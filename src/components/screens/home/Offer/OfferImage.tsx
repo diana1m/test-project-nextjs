7c90e42a56e1fc33bf46d0a4cd3d3ba87a534d04
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-08-18 16:27:47 +0300

    add adaptation

--- src/components/screens/home/Offer/OfferImage.tsx
+++ src/components/screens/home/Offer/OfferImage.tsx
@@ -3,12 +3,13 @@ import Image from "next/image";
 
 import data from '@/data/offer.json'
 import IImage from "@/shared/interfaces/IImage";
+import styles from './Offer.module.scss'
 
 const OfferImage: FC = () => {
     const {src, alt, height, width}: IImage= data.image;
 
     return(
-        <Image src={src} alt={alt} width={width} height={height}/> 
+        <Image className={styles.image} src={src} alt={alt} width={width} height={height}/> 
     )
 }
 
